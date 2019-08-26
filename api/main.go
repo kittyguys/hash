402023104b647766f697f60da06b0c6073f58119
Shogo Yasuda <shgysd@gmail.com>
2019-08-26 23:04:35 +0900

    Add utils, auth files

--- api/main.go
+++ api/main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pbnki",
-		})
-	})
-	r.Run(":8080")
 }
