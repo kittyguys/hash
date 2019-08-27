5cb02c3cb4c2a85b76422f83e67c1aa616b9507d
Shogo Yasuda <shgysd@gmail.com>
2019-08-27 22:38:02 +0900

    MySQLに接続できるようにした

--- api/main.go
+++ api/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/gorilla/mux"
 	"github.com/kittyguys/hash/api/auth"
+	"github.com/kittyguys/hash/api/db"
 )
 
 var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
 	post := &post{
 		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
 		Tag:   "Go",
@@ -25,11 +30,19 @@ type post struct {
 }
 
 func main() {
+	mysql := db.Conn()
+	db.Insert(mysql)
+	defer func() {
+		fmt.Println("Disconnected from db")
+		mysql.Close()
+	}()
+
 	r := mux.NewRouter()
 	// localhost:8080/publicでpublicハンドラーを実行
-	r.Handle("/public", public)
+	r.Handle("/public", public).Methods("GET", "POST")
 	r.Handle("/private", auth.JwtMiddleware.Handler(private))
 	r.Handle("/auth", auth.GetTokenHandler)
+	fmt.Println("Server starts on 8080")
 
 	//サーバー起動
 	if err := http.ListenAndServe(":8080", r); err != nil {
@@ -37,7 +50,20 @@ func main() {
 	}
 }
 
+type User interface{}
+
 var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	var u User
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	fmt.Println(u)
 	post := &post{
 		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
 		Tag:   "Vue.js",
