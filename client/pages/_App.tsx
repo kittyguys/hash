328ee63565e870b2cc004dbcaf1c827360b15084
軽部勝仁 <karube5217@gmail.com>
2019-11-04 22:12:40 +0900

    Nextでは不要になるRoutingを削除する

--- client/pages/_App.tsx
+++ client/pages/_App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
@@ -9,14 +8,6 @@ import {
   myDataChangeFailed
 } from "../src/redux/MyData/action";
 const hashImage = require("./assets/images/hash.jpg");
-
-import Home from "./Home";
-import Signup from "./Signup/Signup";
-import SignupConfirm from "./Signup/SignupConfirm";
-import Signin from "./Signin/Signin";
-import Users from "./Users";
-import Mypage from "./Mypage";
-import User from "./User";
 
 const App: React.FC = () => {
   // ユーザ情報の取得処理
@@ -29,15 +20,6 @@ const App: React.FC = () => {
     }
   }, []);
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/signup/confirm" component={SignupConfirm} />
-      <Route exact path="/signin" component={Signin} />
-      <Route exact path="/users" component={Users} />
-      <Route exact path="/mypage" component={Mypage} />
-      <Route exact path="/user" component={User} />
-    </Router>
   );
 };
 
