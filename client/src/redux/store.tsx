ae19025b3291737297d4ff3ba6e9557cfd20c2df
Shogo Yasuda <shgysd@gmail.com>
2019-11-11 01:23:52 +0900

    sagaを削除

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,47 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { rootSaga } from "./sagas/rootSaga";
-import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import { createStore, combineReducers, compose } from "redux";
 
 // reducers
+import authReducer from "./auth/reducer";
 import signupReducer from "./Signup/reducer";
 import signinReducer from "./Signin/reducer";
 import myDataReducer from "./MyData/reducer";
 import mypageInputReducer from "./MypageInput/reducer";
 import homeInputReducer from "./HomeInput/reducer";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   signup: signupReducer,
   signin: signinReducer,
   myData: myDataReducer,
   mypageInput: mypageInputReducer,
-  homeInput: homeInputReducer
+  homeInput: homeInputReducer,
+  auth: authReducer
 });
 
-const sagaMiddleWare = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleWare));
-const bindMiddleware = (middleware: [SagaMiddleware<object>]) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-  return applyMiddleware(...middleware);
-};
-
-declare module "redux" {
-  export interface Store {
-    sagaTask: any;
-  }
-}
-
 export const configureStore = (initialState = {}) => {
-  const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    initialState,
-    bindMiddleware([sagaMiddleware])
-  );
-
-  store.sagaTask = sagaMiddleware.run(rootSaga);
-
+  const store = createStore(rootReducer, initialState, devToolsEnhancer({}));
   return store;
 };
