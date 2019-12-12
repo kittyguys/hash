de61a4f6dfcd90832117a64d78579db204c10ff0
finalize <shgysd@gmail.com>
2019-12-13 01:49:26 +0900

    WIP: editing settings/profile

--- client/pages/[root].tsx
+++ client/pages/[root].tsx
@@ -1,54 +1,87 @@
 import { NextPage } from "next";
+import Router from "next/router";
 import styled from "styled-components";
+import cookies from "next-cookies";
+import jwt_decode from "jwt-decode";
 import BaseAvatar from "@src/common/components/shared/Avatar";
 import BaseUserName from "@src/common/components/shared/UserName";
+import Header from "@src/common/components/shared/Header";
+import { signinSuccess } from "@src/features/auth/actions";
+import { updateProfileSuccess } from "@src/features/profile/actions";
 
-const User: NextPage = props => {
-  return <></>;
+const Profile: NextPage = () => {
+  const handleEdit = () => {
+    Router.push("/settings/profile");
+  };
+  return (
+    <ProfileWrapper>
+      <Header route="/profile" />
+      <Main>
+        <Avatar />
+        <UserName>山田　貴之</UserName>
+        <Email>your@example.com</Email>
+        <EditButton onClick={handleEdit}>編集</EditButton>
+      </Main>
+    </ProfileWrapper>
+  );
 };
 
-const MypageWrapper = styled.div`
-  width: 640px;
-  margin: 120px auto 0;
-  border: 1px solid #dbdbdb;
-  padding: 30px;
-  @media (max-width: 768px) {
-    padding: 0 20px;
-    width: auto;
-    margin: 0;
-    border: none;
+Profile.getInitialProps = async (ctx: any) => {
+  const allCookies = cookies(ctx);
+  const token = allCookies.jwt;
+  if (typeof token === "string") {
+    const profile = jwt_decode(token);
+    ctx.store.dispatch(signinSuccess());
+    ctx.store.dispatch(updateProfileSuccess(profile));
   }
+  return { store: ctx.store };
+};
+
+const ProfileWrapper = styled.div`
+  padding-top: 84px;
 `;
 
-const MainLayout = styled.div`
-  display: flex;
+const Main = styled.div`
+  width: 1024px;
+  margin: 0 auto;
+  padding-top: 84px;
 `;
 
 const Avatar = styled(BaseAvatar)`
-  width: 90px;
-  height: 90px;
-  @media (max-width: 768px) {
-    width: 60px;
-    height: 60px;
-  }
+  width: 120px;
+  height: 120px;
+  margin: 0 auto 24px;
 `;
 
-const SubLayout = styled.div`
+const UserName = styled.div`
+  color: #555;
+  font-size: 2rem;
+  margin: 0 auto 24px;
+  text-align: center;
+`;
+
+const Email = styled.div`
+  color: #777;
+  font-size: 2rem;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const EditButton = styled.button`
   display: block;
-  margin-left: 20px;
-  margin-top: 20px;
-`;
-
-const UserName = styled(BaseUserName)`
-  font-size: 30px;
-  font-weight: bold;
-  @media (max-width: 768px) {
-    font-size: 24px;
+  background-color: #6b52ae;
+  color: #fff;
+  width: 320px;
+  height: 38px;
+  margin: 12px auto 0;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  font-size: 1.6rem;
+  &:hover {
+    opacity: 0.7;
+    cursor: pointer;
   }
 `;
 
-const TagBoxLayout = styled.div`
-  margin-top: 20px;
-`;
-
-export default User;
+export default Profile;
--- client/pages/settings/profile.tsx
+++ client/pages/settings/profile.tsx
@@ -1,59 +1,105 @@
 import { NextPage } from "next";
 import styled from "styled-components";
 import BaseAvatar from "@src/common/components/shared/Avatar";
-import BaseUserName from "@src/common/components/shared/UserName";
 import Header from "@src/common/components/shared/Header";
 
-const User: NextPage = props => {
+const Profile: NextPage = () => {
+  const handleEdit = () => {};
   return (
-    <>
+    <Wrapper>
       <Header />
-    </>
+      <Main>
+        <Avatar />
+        <UserName>山田　貴之</UserName>
+        <Email>your@example.com</Email>
+        <EditButton onClick={handleEdit}>編集</EditButton>
+        <SideBar>
+          <SettingOption active>プロフィール</SettingOption>
+          <SettingOption>Apple</SettingOption>
+          <SettingOption>Spotify</SettingOption>
+          <SettingOption>Disney</SettingOption>
+          <SettingOption>Google</SettingOption>
+          <SettingOption>Amazon</SettingOption>
+          <SettingOption>Facebook</SettingOption>
+          <SettingOption>Netflix</SettingOption>
+          <SettingOption>Slack</SettingOption>
+          <SettingOption>Tesla</SettingOption>
+        </SideBar>
+      </Main>
+    </Wrapper>
   );
 };
 
-const MypageWrapper = styled.div`
-  width: 640px;
-  margin: 120px auto 0;
-  border: 1px solid #dbdbdb;
-  padding: 30px;
-  @media (max-width: 768px) {
-    padding: 0 20px;
-    width: auto;
-    margin: 0;
-    border: none;
-  }
+const Wrapper = styled.div`
+  padding-top: 84px;
 `;
 
-const MainLayout = styled.div`
-  display: flex;
+const Main = styled.div`
+  width: 1024px;
+  margin: 0 auto;
+  padding-top: 84px;
 `;
 
 const Avatar = styled(BaseAvatar)`
-  width: 90px;
-  height: 90px;
-  @media (max-width: 768px) {
-    width: 60px;
-    height: 60px;
-  }
+  width: 120px;
+  height: 120px;
+  margin: 0 auto 24px;
 `;
 
-const SubLayout = styled.div`
+const UserName = styled.div`
+  color: #555;
+  font-size: 2rem;
+  margin: 0 auto 24px;
+  text-align: center;
+`;
+
+const Email = styled.div`
+  color: #777;
+  font-size: 2rem;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const EditButton = styled.button`
   display: block;
-  margin-left: 20px;
-  margin-top: 20px;
-`;
-
-const UserName = styled(BaseUserName)`
-  font-size: 30px;
-  font-weight: bold;
-  @media (max-width: 768px) {
-    font-size: 24px;
+  background-color: #6b52ae;
+  color: #fff;
+  width: 320px;
+  height: 38px;
+  margin: 12px auto 0;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  font-size: 1.6rem;
+  &:hover {
+    opacity: 0.7;
+    cursor: pointer;
   }
 `;
 
-const TagBoxLayout = styled.div`
-  margin-top: 20px;
+const SideBar = styled.div`
+  width: 280px;
+  box-shadow: 0 1px 3px 0 rgba(32, 33, 36, 0.28);
+  border-radius: 8px;
 `;
 
-export default User;
+const SettingOption = styled.div<{ active?: boolean }>`
+  cursor: pointer;
+  color: #555;
+  font-size: 1.6rem;
+  padding: 12px 0;
+  text-align: center;
+  font-weight: ${({ active }) => (active ? "bold" : "none")};
+  &:hover {
+    color: #fff;
+    background-color: #555;
+  }
+  &:first-child {
+    border-radius: 8px 8px 0 0;
+  }
+  &:last-child {
+    border-radius: 0 0 8px 8px;
+  }
+`;
+
+export default Profile;
