12530bc0c734fd806f3f9fd07110ec6179a7fe3a
finalize <shgysd@gmail.com>
2019-12-15 18:05:19 +0900

    SSRログインに対応

--- client/pages/settings/profile.tsx
+++ client/pages/settings/profile.tsx
@@ -1,23 +1,34 @@
 import { NextPage } from "next";
 import styled from "styled-components";
+import cookies from "next-cookies";
+import jwt_decode from "jwt-decode";
 import Header from "@src/common/components/shared/Header";
 import { SideBar, ProfileEditor } from "@src/common/components/pages/settings";
+import { signinSuccess } from "@src/features/auth/actions";
+import { updateProfileSuccess } from "@src/features/profile/actions";
 
 const Profile: NextPage = () => {
   return (
-    <Wrapper>
+    <>
       <Header />
       <Main>
         <SideBar />
         <ProfileEditor />
       </Main>
-    </Wrapper>
+    </>
   );
 };
 
-const Wrapper = styled.div`
-  padding-top: 144px;
-`;
+Profile.getInitialProps = async (ctx: any) => {
+  const allCookies = cookies(ctx);
+  const token = allCookies.jwt;
+  if (typeof token === "string") {
+    const profile = jwt_decode(token);
+    ctx.store.dispatch(signinSuccess());
+    ctx.store.dispatch(updateProfileSuccess(profile));
+  }
+  return { store: ctx.store };
+};
 
 const Main = styled.div`
   width: 1024px;
