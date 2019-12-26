8397e3deae9a53d33a170ea58c0dbfebad09e6ac
finalize <shgysd@gmail.com>
2019-12-27 01:39:10 +0900

    Bug fix: signin API

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import sql from "../configs/mysql";
+import pool from "../configs/mysql";
 
 export const signup = (req, res, next) => {
   try {
@@ -15,7 +15,7 @@ export const signup = (req, res, next) => {
       };
       sql.query("insert into users set ?", query, (err, result) => {
         if (err) {
-          throw err
+          throw err;
         } else {
           if (result) {
             const user = {
@@ -35,36 +35,38 @@ export const signup = (req, res, next) => {
   }
 };
 
-export const signin = (req, res, next) => {
+export const signin = async (req, res, next) => {
+  let connection;
   try {
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
     const password = req.body.password;
     const signinID = req.body.signinID;
-    sql.query(
-      "SELECT * FROM users WHERE user_name = ? LIMIT 1",
-      [signinID],
-      (err, users) => {
-        console.log(users)
-        if (err) {
-          throw err
-        } else {
-          bcrypt.compare(password, users[0].password, (err, result) => {
-            if (err) {
-              throw err;
-            }
-            if (result) {
-              const user = {
-                id: users[0].id,
-                user_name: users[0].user_name,
-                display_name: users[0].display_name,
-                email: users[0].email
-              };
-              const token = jwt.sign(user, process.env.JWT_SECRET_KEY);
-              return res.json({ token });
-            }
-          });
-        }
+    const rows = await connection
+      .query("SELECT * FROM users WHERE user_name = ? LIMIT 1", [signinID])
+      .then(data => {
+        return data[0];
+      })
+      .catch(err => {
+        next(err);
+      });
+    bcrypt.compare(password, rows[0].password, (err, result) => {
+      if (err) {
+        throw err;
       }
-    );
+      if (result) {
+        const user = {
+          id: rows[0].id,
+          user_name: rows[0].user_name,
+          display_name: rows[0].display_name,
+          email: rows[0].email
+        };
+        const token = jwt.sign(user, process.env.JWT_SECRET_KEY);
+        return res.json({ token });
+      } else {
+        return res.json({ error: "パスワードが間違ってるぞコラ" });
+      }
+    });
   } catch (err) {
     next(err);
   }
